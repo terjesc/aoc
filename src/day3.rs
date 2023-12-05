eddfd8e2f6632c2f88c9f994b012b8ae890f61cd
Terje Schjelderup <terjesc@pvv.org>
2023-12-05 23:30:39 +0100

    Add day 3 part 2

--- src/day3.rs
+++ src/day3.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 pub fn solve(input: String) {
 
     // APPROACH
@@ -48,6 +50,41 @@ pub fn solve(input: String) {
         }
     }
 
+    fn neighbors(n: &PartNumber, dimensions: (usize, usize)) -> Vec<(usize, usize)> {
+        let mut neighbors: Vec<(usize, usize)> = Vec::new();
+
+        let x_span = (
+            if n.position.0 > 0 { n.position.0 - 1 } else { 0 },
+            if n.position.0 + n.length < dimensions.0 {
+                n.position.0 + n.length
+            } else {
+                n.position.0 + n.length - 1
+            },
+        );
+
+        if n.position.1 > 0 {
+            for x in x_span.0..=x_span.1 {
+                neighbors.push((x, n.position.1 - 1));
+            }
+        }
+
+        if n.position.1 + 1 < dimensions.1 {
+            for x in x_span.0..=x_span.1 {
+                neighbors.push((x, n.position.1 + 1));
+            }
+        }
+
+        if x_span.0 < n.position.0 {
+            neighbors.push((x_span.0, n.position.1))
+        }
+
+        if x_span.1 >= n.position.0 + n.length {
+            neighbors.push((x_span.1, n.position.1))
+        }
+
+        neighbors
+    }
+
     // Filter numbers based on the occurence of special symbols around them
     let valid_part_numbers: Vec<PartNumber> = all_numbers.into_iter()
             .filter(|n| {
@@ -55,54 +92,50 @@ pub fn solve(input: String) {
                     let character = grid[y][x];
                     !character.is_digit(10) && character != '.'
                 };
-
-                let x_span = (
-                    if n.position.0 > 0 { n.position.0 - 1 } else { 0 },
-                    if n.position.0 + n.length < dimensions.0 {
-                        n.position.0 + n.length
-                    } else {
-                        n.position.0 + n.length - 1
-                    },
-                );
-
-                if n.position.1 > 0 {
-                    for x in x_span.0..=x_span.1 {
-                        if validates_number(x, n.position.1 - 1) {
-                            return true;
-                        }
-                    }
-                }
-
-                if n.position.1 + 1 < dimensions.1 {
-                    for x in x_span.0..=x_span.1 {
-                        if validates_number(x, n.position.1 + 1) {
-                            return true;
-                        }
-                    }
-                }
-
-                if x_span.0 < n.position.0 {
-                    if validates_number(x_span.0, n.position.1) {
-                        return true;
-                    }
-                }
-
-                if x_span.1 >= n.position.0 + n.length {
-                    if validates_number(x_span.1, n.position.1) {
-                        return true;
-                    }
-                }
-
-                false
+                neighbors(n, dimensions).iter().any(|(x, y)| validates_number(*x, *y))
             })
             .collect();
 
     let part1: u32 = valid_part_numbers.iter().map(|n| n.value).sum();
 
     println!("Day 3 part 1: {}", part1);
+    // 544433
 
     // Approach part 2:
     // Identify all cog positions ('*' characters)
     // Find numbers around them
     // If more than one number: Multiply the numbers and add it to the sum.
+
+    // Locate the cogs,
+    // and prepare an (empty) index from cog positions to neighboring numbers
+    let mut cog_positions: HashMap<(usize, usize), Vec<usize>> = HashMap::new();
+    for (y_index, row) in grid.iter().enumerate() {
+        for (x_index, character) in row.iter().enumerate() {
+            if *character == '*' {
+                cog_positions.insert((x_index, y_index), Vec::new());
+            }
+        }
+    }
+
+    // Index neighboring numbers
+    for number in &valid_part_numbers {
+        for neighbor in neighbors(&number, dimensions) {
+            if let Some(n) = cog_positions.get_mut(&neighbor) {
+                n.push(number.value as usize);
+            }
+        }
+    }
+
+    // For each cog position, if more than one number, multiply them and sum.
+    let part2: usize = cog_positions.iter()
+            .map(|(_, numbers)| {
+                if numbers.len() >= 2 {
+                    numbers.iter().copied().reduce(|acc, e| acc * e).unwrap()
+                } else {
+                    0
+                }
+            })
+            .sum();
+
+    println!("Day 3 part 2: {}", part2);
 }
