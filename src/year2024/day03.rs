f0e73351b6b4531afacfcc138f098265ddc2e38d
Terje Schjelderup <terjesc@pvv.org>
2024-12-05 20:55:28 +0100

    Add year 2024 day 3

---
+++ src/year2024/day03.rs
@@ -0,0 +1,52 @@
+use regex::Regex;
+
+pub fn solve(input: String) {
+    // Regex for matching mul(a, b) commands. #1 = a, #2 = b.
+    let re = Regex::new(r"mul\(([0-9]{1,3}),([0-9]{1,3})\)").unwrap();
+
+    // Add up the results from all properly formatted mul() commands
+    let part1: usize = re
+        .captures_iter(&input)
+        .map(|capture| capture[1].parse::<usize>().unwrap() * capture[2].parse::<usize>().unwrap())
+        .sum();
+
+    println!("Day 1 part 1: {}", part1);
+
+    // Find the parts of the input that should be included.
+    // First up to the first "don't()".
+    // Then repeatedly from the first following "do()",
+    // to the next following "don't" or end of input.
+    let mut include_intervals: Vec<(usize, usize)> = Vec::new();
+
+    let first_dont_index = input.find("don't()").unwrap_or(input.len());
+    include_intervals.push((0, first_dont_index));
+
+    let mut index = first_dont_index;
+    while let Some(start) = input
+        .get(index..)
+        .and_then(|s| s.find("do()").map(|i| i + index))
+    {
+        index = start;
+        let end = input
+            .get(index..)
+            .and_then(|s| s.find("don't()").map(|i| i + index))
+            .unwrap_or(input.len());
+        index = end;
+
+        include_intervals.push((start, end));
+    }
+
+    // Add up the results from only the mul() commands within the proper intervals
+    let part2: usize = include_intervals
+        .iter()
+        .map(|&(start, end)| {
+            re.captures_iter(&input[start..end])
+                .map(|capture| {
+                    capture[1].parse::<usize>().unwrap() * capture[2].parse::<usize>().unwrap()
+                })
+                .sum::<usize>()
+        })
+        .sum();
+
+    println!("Day 1 part 2: {}", part2);
+}
--- src/year2024/mod.rs
+++ src/year2024/mod.rs
@@ -1,10 +1,12 @@
 mod day01;
 mod day02;
+mod day03;
 
 pub fn solve(input: String, day: u8) {
     match day {
         1 => day01::solve(input),
         2 => day02::solve(input),
+        3 => day03::solve(input),
         _ => unimplemented!(),
     }
 }
